e37d743eaa9840c34efaf00c380e9bd271ad4b0f
Craig Crawford <craigcrawford1988@gmail.com>
2017-09-18 19:37:06 +0100

    Create sqlite-to-mariadb-alt.pl
    This file is for when your DB is for TS3 and you have backslashes in the channel paths which causes an error when you try to import the DB after running the mariadb-ts3-fixes.sh file. You will need to re-export the DB, then run this and then finally the TS3 fixes.

---
+++ sqlite-to-mariadb-alt.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+#This file is for when your DB is for TS3 and you have backslashes in the channel paths which causes an error when you try to import the DB like:
+while ($line = <>){
+    if (($line !~  /BEGIN TRANSACTION/) && ($line !~ /COMMIT/) && ($line !~ /sqlite_sequence/) && ($line !~ /CREATE UNIQUE INDEX/) && ($line !~ /PRAGMA foreign_keys=OFF/)){
+ 
+        if ($line =~ /CREATE TABLE \"(\w*)\"(.*)/){
+                $name = $1;
+                $sub = $2;
+                $sub =~ s/\"//g;
+                $line = "DROP TABLE IF EXISTS $name;\nCREATE TABLE IF NOT EXISTS $name$sub\n";
+        }
+        elsif ($line =~ /INSERT INTO \"(\w*)\"(.*)/){
+                $line = "INSERT INTO $1$2\n";
+                $line =~ s/\"/\\\"/g;
+                $line =~ s/\"/\'/g;
+        }else{
+                $line =~ s/\'\'/\\\'/g;
+        }
+        $line =~ s/([^\\'])\'t\'(.)/$1THIS_IS_TRUE$2/g;
+        $line =~ s/THIS_IS_TRUE/1/g;
+        $line =~ s/([^\\'])\'f\'(.)/$1THIS_IS_FALSE$2/g;
+        $line =~ s/THIS_IS_FALSE/0/g;
+        $line =~ s/AUTOINCREMENT/AUTO_INCREMENT/g;
+        $line =~ s/(.*)varchar([^\(0-9\)].*)/$1varchar\(255\)$2/g;
+        print $line;
+    }
+}
